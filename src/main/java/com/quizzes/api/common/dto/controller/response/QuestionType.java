cba8c29ceff24791b55bb53e6f3fde7db6098930
Jeffrey A. Bermudez P <jeffrey.bermudez@edify.cr>
2016-11-09 16:41:10 -0600

    QZ-000_quick_fix_get_collection: Implementing a quick fix for a specific response data required by FE team.

--- src/main/java/com/quizzes/api/common/dto/controller/response/QuestionType.java
+++ src/main/java/com/quizzes/api/common/dto/controller/response/QuestionType.java
@@ -1,15 +1,16 @@
 package com.quizzes.api.common.dto.controller.response;
 
 public enum QuestionType {
-    TrueFalse ("TrueFalse"),
-    SingleChoice ("SingleChoice"),
-    MultipleChoice ("MultipleChoice"),
-    MultipleImage ("MultipleImage"),
-    Match ("Match");
+    TrueFalse ("true_false"),
+    SingleChoice ("single_choice"),
+    MultipleChoice ("multiple_choice"),
+    MultipleImage ("multiple_image"),
+    Match ("match");
 
     private final java.lang.String literal;
 
     private QuestionType(java.lang.String literal) {
         this.literal = literal;
     }
+
 }
