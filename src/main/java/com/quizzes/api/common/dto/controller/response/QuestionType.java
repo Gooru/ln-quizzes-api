ee07192ead9fd06295f3fbaf387d66a258c80f61
Jeffrey A. Bermudez P <jeffrey.bermudez@edify.cr>
2016-11-09 18:10:32 -0600

    QZ-000_quick_fix_get_collection_serializer: Fixing enum serializer.

--- src/main/java/com/quizzes/api/common/dto/controller/response/QuestionType.java
+++ src/main/java/com/quizzes/api/common/dto/controller/response/QuestionType.java
@@ -1,10 +1,22 @@
 package com.quizzes.api.common.dto.controller.response;
 
+import com.google.gson.annotations.SerializedName;
+
 public enum QuestionType {
+
+    @SerializedName("true_false")
     TrueFalse ("true_false"),
+
+    @SerializedName("single_choice")
     SingleChoice ("single_choice"),
+
+    @SerializedName("multiple_choice")
     MultipleChoice ("multiple_choice"),
+
+    @SerializedName("multiple_image")
     MultipleImage ("multiple_image"),
+
+    @SerializedName("match")
     Match ("match");
 
     private final java.lang.String literal;
