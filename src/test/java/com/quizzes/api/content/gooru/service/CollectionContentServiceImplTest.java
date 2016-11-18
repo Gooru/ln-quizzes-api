e428e006c6fd644306ab74fed4b14432a9915e1f
Jeffrey A. Bermudez P <jeffrey.bermudez@edify.cr>
2016-11-18 10:25:39 -0600

    QZ-178: Implementation for mapping all the answers of each question in the collection and saving them to the DB.

--- src/test/java/com/quizzes/api/content/gooru/service/CollectionContentServiceImplTest.java
+++ src/test/java/com/quizzes/api/content/gooru/service/CollectionContentServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.quizzes.api.content.gooru.service;
 
+import com.quizzes.api.common.enums.QuestionTypeEnum;
 import com.quizzes.api.common.model.tables.pojos.Collection;
 import com.quizzes.api.common.model.tables.pojos.Profile;
 import com.quizzes.api.common.model.tables.pojos.Resource;
@@ -16,10 +17,13 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.internal.WhiteboxImpl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
@@ -30,7 +34,8 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(CollectionContentService.class)
 public class CollectionContentServiceImplTest {
 
     @InjectMocks
@@ -47,7 +52,6 @@ public class CollectionContentServiceImplTest {
 
     @Test
     public void createCollectionCopy() throws Exception {
-
         AnswerDto answerMultipleChoice1 = new AnswerDto();
         answerMultipleChoice1.setAnswerText("Answer Multiple Choice 1 text");
         answerMultipleChoice1.setIsCorrect("false");
@@ -81,13 +85,13 @@ public class CollectionContentServiceImplTest {
         answerTrueFalse1.setSequence(1);
 
         AnswerDto answerTrueFalse2 = new AnswerDto();
-        answerTrueFalse2.setAnswerText("Answer True False 1 text");
-        answerTrueFalse2.setIsCorrect("true");
-        answerTrueFalse2.setSequence(1);
+        answerTrueFalse2.setAnswerText("Answer True False 2 text");
+        answerTrueFalse2.setIsCorrect("false");
+        answerTrueFalse2.setSequence(2);
 
         List<AnswerDto> answerTrueFalseList = new ArrayList<>();
         answerTrueFalseList.add(answerTrueFalse1);
-        answerTrueFalseList.add(answerTrueFalse1);
+        answerTrueFalseList.add(answerTrueFalse2);
 
         QuestionDto questionTrueFalse = new QuestionDto();
         questionTrueFalse.setId(UUID.randomUUID().toString());
@@ -123,8 +127,75 @@ public class CollectionContentServiceImplTest {
         verify(collectionRestClient, times(1)).getAssessment(externalCollectionId);
         verify(collectionService, times(1)).save(any(Collection.class));
         verify(resourceService, atLeast(1)).save(any(Resource.class));
+
         assertNotNull("Copied Collection is null", copiedCollection);
         assertEquals("Copied Collection ID is different", collection.getId(), copiedCollection.getId());
+    }
+
+    @Test
+    public void createInteraction() throws Exception {
+        AnswerDto answerTrueFalse1 = new AnswerDto();
+        answerTrueFalse1.setAnswerText("Answer True False 1 text");
+        answerTrueFalse1.setIsCorrect("true");
+        answerTrueFalse1.setSequence(1);
+        AnswerDto answerTrueFalse2 = new AnswerDto();
+        answerTrueFalse2.setAnswerText("Answer True False 1 text");
+        answerTrueFalse2.setIsCorrect("false");
+        answerTrueFalse2.setSequence(2);
+        List<AnswerDto> answers = new ArrayList<>();
+        answers.add(answerTrueFalse1);
+        answers.add(answerTrueFalse2);
+
+        Map<String, Object> interaction =
+                WhiteboxImpl.invokeMethod(collectionContentService, "createInteraction", answers);
+
+        assertEquals("Wrong value for interaction shuffle", false, interaction.get("shuffle"));
+        assertEquals("Wrong value for interaction maxChoices", 0, interaction.get("maxChoices"));
+        assertEquals("Wrong value for interaction prompt", "", interaction.get("prompt"));
+        List<Map<String, Object>> choices =  (List<Map<String, Object>>) interaction.get("choices");
+        assertEquals("Wrong number of interaction choices", 2, choices.size());
+        assertEquals("Wrong value for choice text", answerTrueFalse1.getAnswerText(), choices.get(0).get("text"));
+        assertEquals("Wrong value for choice value", answerTrueFalse1.getAnswerText(), choices.get(0).get("value"));
+        assertEquals("Wrong value for choice sequence", answerTrueFalse1.getSequence(), choices.get(0).get("sequence"));
+        assertEquals("Wrong value for choice isFixed", true, choices.get(0).get("isFixed"));
+    }
+
+    @Test
+    public void mapQuestionType() throws Exception {
+        String trueFalseQuestionType = WhiteboxImpl.invokeMethod(collectionContentService, "mapQuestionType",
+                GooruQuestionTypeEnum.TrueFalseQuestion.getLiteral());
+        assertEquals("True/False question type wrongly mapped",
+                QuestionTypeEnum.TrueFalse.getLiteral(), trueFalseQuestionType);
+        String singleChoiceQuestionType = WhiteboxImpl.invokeMethod(collectionContentService, "mapQuestionType",
+                GooruQuestionTypeEnum.MultipleChoiceQuestion.getLiteral());
+        assertEquals("MultipleChoice question type wrongly mapped",
+                QuestionTypeEnum.SingleChoice.getLiteral(), singleChoiceQuestionType);
+        String noneQuestionType = WhiteboxImpl.invokeMethod(collectionContentService, "mapQuestionType",
+                "unknown");
+        assertEquals("None question type wrongly mapped",
+                QuestionTypeEnum.None.getLiteral(), noneQuestionType);
+    }
+
+    @Test
+    public void getCorrectAnswers() throws Exception {
+        AnswerDto answerTrueFalse1 = new AnswerDto();
+        answerTrueFalse1.setAnswerText("Answer True False 1 text");
+        answerTrueFalse1.setIsCorrect("true");
+        answerTrueFalse1.setSequence(1);
+        AnswerDto answerTrueFalse2 = new AnswerDto();
+        answerTrueFalse2.setAnswerText("Answer True False 1 text");
+        answerTrueFalse2.setIsCorrect("false");
+        answerTrueFalse2.setSequence(2);
+        List<AnswerDto> answers = new ArrayList<>();
+        answers.add(answerTrueFalse1);
+        answers.add(answerTrueFalse2);
+
+        List<Map<String, String>> correctAnswers =
+                WhiteboxImpl.invokeMethod(collectionContentService, "getCorrectAnswers", answers);
+
+        assertEquals("Wrong number of correct answers", 1, correctAnswers.size());
+        assertEquals("Wrong answer value",
+                answerTrueFalse1.getAnswerText(), correctAnswers.get(0).get("value"));
     }
 
 }
