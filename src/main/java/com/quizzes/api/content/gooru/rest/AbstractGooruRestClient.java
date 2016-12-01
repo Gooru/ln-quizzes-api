f045a87d2f6b32cebcb3ca53e559a95039d91241
Fabian Perez <fabian@gooru.org>
2016-12-01 10:43:20 -0600

    QZ-78: Integration with Gooru's endpoint to obtain valid User Token

--- src/main/java/com/quizzes/api/content/gooru/rest/AbstractGooruRestClient.java
+++ src/main/java/com/quizzes/api/content/gooru/rest/AbstractGooruRestClient.java
@@ -6,6 +6,8 @@ import com.quizzes.api.common.exception.ContentProviderException;
 import com.quizzes.api.common.exception.InternalServerException;
 import com.quizzes.api.content.gooru.dto.TokenRequestDto;
 import com.quizzes.api.content.gooru.dto.TokenResponseDto;
+import com.quizzes.api.content.gooru.dto.TokenUserRequestDto;
+import com.quizzes.api.content.gooru.dto.UserDataTokenDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,7 @@ public abstract class AbstractGooruRestClient {
 
     private static final String CLIENT_KEY = "c2hlZWJhbkBnb29ydWxlYXJuaW5nLm9yZw==";
     private static final String CLIENT_ID = "ba956a97-ae15-11e5-a302-f8a963065976";
+    private static final String RETURN_URL = "http://www.gooru.org";
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -28,6 +31,37 @@ public abstract class AbstractGooruRestClient {
 
     @Autowired
     private Gson gsonPretty;
+
+    public String generateUserToken(UserDataTokenDto user) {
+        String endpointUrl = getContentApiUrl() + "/api/nucleus-auth/v1/authorize";
+
+        TokenUserRequestDto tokenUserRequest =
+                new TokenUserRequestDto(CLIENT_KEY, CLIENT_ID, "google", RETURN_URL, user);
+
+        if (logger.isDebugEnabled()) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            logger.debug("POST Request to: " + endpointUrl);
+            logger.debug("Body: " + gson.toJson(tokenUserRequest));
+        }
+
+        try {
+            TokenResponseDto tokenResponse =
+                    restTemplate.postForObject(endpointUrl, tokenUserRequest, TokenResponseDto.class);
+
+            if (logger.isDebugEnabled()) {
+                logger.debug("Response from: " + endpointUrl);
+                logger.debug("Body: " + gsonPretty.toJson(tokenResponse));
+            }
+
+            return tokenResponse.getToken();
+        } catch (RestClientException rce) {
+            logger.error("User token could not be generated.", rce);
+            throw new ContentProviderException("User token could not be generated.", rce);
+        } catch (Exception e) {
+            logger.error("User token could not be generated.", e);
+            throw new InternalServerException("User token could not be generated.", e);
+        }
+    }
 
     public String generateAnonymousToken() {
         String endpointUrl = getContentApiUrl() + "/api/nucleus-auth/v1/token";
---
+++ src/test/java/com/quizzes/api/content/gooru/rest/AbstractGooruRestClientTest.java
@@ -0,0 +1,65 @@
+package com.quizzes.api.content.gooru.rest;
+
+import com.quizzes.api.common.exception.InternalServerException;
+import com.quizzes.api.content.gooru.dto.TokenResponseDto;
+import com.quizzes.api.content.gooru.dto.TokenUserRequestDto;
+import com.quizzes.api.content.gooru.dto.UserDataTokenDto;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doReturn;
+import static org.testng.AssertJUnit.assertEquals;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class AbstractGooruRestClientTest {
+
+    @InjectMocks
+    private AbstractGooruRestClient abstractGooruRestClient = Mockito.spy(AbstractGooruRestClient.class);
+
+    @Mock
+    RestTemplate restTemplate;
+
+    @Value("${content.api.url}")
+    private String contentApiUrl;
+
+    @Test
+    public void generateUserToken() throws Exception {
+        UserDataTokenDto user = new UserDataTokenDto();
+        user.setFirstName("Gooru");
+        user.setLastName("My");
+        user.setIdentityId("mygooru@quizzes.com");
+
+        String userToken = UUID.randomUUID().toString();
+        TokenResponseDto token = new TokenResponseDto();
+        token.setToken(userToken);
+
+        doReturn(token).when(restTemplate)
+                .postForObject(any(String.class), any(TokenUserRequestDto.class), eq(TokenResponseDto.class));
+
+        String result = abstractGooruRestClient.generateUserToken(user);
+        assertNotNull("Result is null", result);
+        assertEquals("Wrong token", userToken, result);
+    }
+
+    @Test(expected = InternalServerException.class)
+    public void generateUserTokenNull() throws Exception {
+
+        doReturn(null).when(restTemplate)
+                .postForObject(any(String.class), any(TokenUserRequestDto.class), eq(TokenResponseDto.class));
+
+        String result = abstractGooruRestClient.generateUserToken(new UserDataTokenDto());
+    }
+
+}
