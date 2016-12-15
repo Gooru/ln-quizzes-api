905fa2b268a10592d469665de4e03bc5094d0876
mzumbadoate <mzumbado@stormpath.com>
2016-12-15 10:47:59 -0600

    QZ-240 fixing collection constraint

--- src/main/resources/db/migration/V4__collection_drop_index_add_constraint.sql
+++ src/main/resources/db/migration/V4__collection_drop_index_add_constraint.sql
@@ -3,4 +3,4 @@ DROP INDEX IF EXISTS collection_owner_profile_id_idx;
 
 -- this constraint means that only one "owner - external parent" combination is possible by collection
 ALTER TABLE collection
-ADD CONSTRAINT collection_owner_profile_id_external_parent_id_uc UNIQUE (owner_profile_id, DECODE(MD5(external_parent_id), 'HEX'));
+  ADD CONSTRAINT collection_owner_profile_id_external_parent_id_uc UNIQUE (owner_profile_id, external_parent_id);
