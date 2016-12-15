3cb64b709b7e976bc01dadf5e60928e5adc90966
mzumbadoate <mzumbado@stormpath.com>
2016-12-15 13:16:49 -0600

    QZ-240 adding new unique index on collection by owner and external parent id

--- src/main/resources/db/migration/V4__collection_drop_index_add_constraint.sql
+++ src/main/resources/db/migration/V4__collection_drop_index_add_constraint.sql
@@ -1,6 +1,5 @@
 -- the constraint adding below implicitly creates an index with owner_profile_id, so we drop this one
 DROP INDEX IF EXISTS collection_owner_profile_id_idx;
 
--- this constraint means that only one "owner - external parent" combination is possible by collection
-ALTER TABLE collection
-  ADD CONSTRAINT collection_owner_profile_id_external_parent_id_uc UNIQUE (owner_profile_id, external_parent_id);
+-- this index means that only one "owner - external parent" combination is possible
+CREATE UNIQUE INDEX collection_owner_profile_id_external_parent_id_idx ON collection (owner_profile_id, DECODE(MD5(external_parent_id), 'HEX'));
