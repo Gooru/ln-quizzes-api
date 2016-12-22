121085cf519647f0d04afcaad17983113077cd0c
Jeffrey A. Bermudez P <jeffrey.bermudez@edify.cr>
2016-12-22 10:27:34 -0600

    QZ-183: Modifying Jmeter file to be used by BlazeMeter to measure App performance in our current QA server.

--- src/main/java/com/quizzes/api/common/dto/ContextAssignedGetResponseDto.java
+++ src/main/java/com/quizzes/api/common/dto/ContextAssignedGetResponseDto.java
@@ -1,18 +1,12 @@
 package com.quizzes.api.common.dto;
 
-import com.google.gson.annotations.SerializedName;
-import io.swagger.annotations.ApiModelProperty;
-
-import java.util.Map;
-
 public class ContextAssignedGetResponseDto extends CommonContextGetResponseDto {
 
     private IdResponseDto owner;
 
     private boolean hasStarted = false;
 
-    public ContextAssignedGetResponseDto() {
-    }
+    public ContextAssignedGetResponseDto() {}
 
     public IdResponseDto getOwner() {
         return owner;
