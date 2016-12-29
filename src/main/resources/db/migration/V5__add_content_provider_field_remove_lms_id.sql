8bd683bad1943dc544274004fa44586de23bea01
mzumbadoate <mzumbado@stormpath.com>
2016-12-29 12:32:00 -0600

    QZ-320 sql fix

--- src/main/resources/db/migration/V5__add_content_provider_field_remove_lms_id.sql
+++ src/main/resources/db/migration/V5__add_content_provider_field_remove_lms_id.sql
@@ -4,6 +4,6 @@ ALTER TABLE resource ADD COLUMN content_provider content_provider NOT NULL DEFAU
 UPDATE collection SET content_provider=cast(cast(lms_id as VARCHAR) as content_provider);
 UPDATE resource SET content_provider=cast(cast(lms_id as VARCHAR) as content_provider);
 
-ALTER TABLE collection DROP COLUMN lms_id;
-ALTER TABLE resource DROP COLUMN lms_id;
+ALTER TABLE collection DROP COLUMN IF EXISTS lms_id;
+ALTER TABLE resource DROP COLUMN IF EXISTS lms_id;
 
