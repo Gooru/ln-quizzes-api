6f55fe4fc5c3d3cd3322c420e03d75553e5962a6
mzumbadoate <mzumbado@stormpath.com>
2016-12-29 11:17:01 -0600

    QZ-308 adding test, when update has no assignees

--- integration/spec/quizzes-api/updateContext_spec.js
+++ integration/spec/quizzes-api/updateContext_spec.js
@@ -147,3 +147,137 @@ frisby.create('Test Context create with two assignees')
             .toss();
     })
     .toss();
+
+//Testing the update context with NO assignees
+// first we create a context with 2 assignees
+// then we update the context without assignees');
+frisby.create('Test Context create with two assignees')
+    .post(QuizzesApiUrl + '/v1/context', {
+        'externalCollectionId': '927ec170-f7b3-46c3-ace3-24fd61dda0c0',
+        'assignees': [
+            {
+                'id': 'student-id-6',
+                'firstName': 'StudentFirstName6',
+                'lastName': 'StudentLastName6',
+                'username': 'student6',
+                'email': 'student6@quizzes.com'
+            },
+            {
+                'id': 'student-id-7',
+                'firstName': 'StudentFirstName7',
+                'lastName': 'StudentLastName7',
+                'username': 'student7',
+                'email': 'student7@quizzes.com'
+            }
+
+        ],
+        'contextData': {
+            'contextMap': {
+                'classId': 'class-id-1'
+            },
+            'metadata': {}
+        },
+        'owner': {
+            'id': 'teacher-id-1',
+            'firstName': 'TeacherFirstName1',
+            'lastName': 'TeacherLastName1',
+            'username': 'teacher1',
+            'email': 'teacher1@quizzes.com'
+        }
+    }, {json: true})
+    // This profile-id header for this specific endpoint is fake, it is useless but it is required by the endpoint
+    // Both headers will be replaced by the session-token header once we finish with the Session implementation
+    .addHeader('profile-id', '1fd8b1bc-65de-41ee-849c-9b6f339349c9')
+    .addHeader('lms-id', 'quizzes')
+    .inspectRequest()
+
+    .expectStatus(200)
+    .expectHeaderContains('content-type', 'application/json')
+    .inspectJSON()
+    .afterJSON(function (contextJson) {
+        frisby.create('Gets the owner profileId of the created context')
+            .get(QuizzesApiUrl + '/v1/profile-by-external-id/teacher-id-1')
+            .addHeader('lms-id', 'quizzes')
+            .inspectJSON()
+            .afterJSON(function (ownerProfileJson) {
+                frisby.create('Verify that the context exists as expected')
+                    .get(QuizzesApiUrl + '/v1/context/created/' + contextJson.id)
+                    .addHeader('lms-id', 'quizzes')
+                    .addHeader('profile-id', ownerProfileJson.id)
+                    .inspectRequest()
+
+                    .expectStatus(200)
+                    .inspectJSON()
+                    .expectJSON({
+                        'assignees': function (val) {
+                            expect(val.length).toBe(2)
+                        },
+                        'contextData': {
+                            'contextMap': {
+                                'classId': 'class-id-1'
+                            },
+                            'metadata': {
+                                'startDate': 0,
+                                'dueDate': 0
+                            }
+                        },
+                        'id': contextJson.id,
+                        'modifiedDate': function (val) {
+                            expect(val).toBeType(Number)
+                        },
+                        'createdDate': function (val) {
+                            expect(val).toBeType(Number)
+                        }
+                    })
+                    .afterJSON(function (contextDetailsJson) {
+                        frisby.create('Updating the context with NO assignees')
+                            .put(QuizzesApiUrl + '/v1/context/' + contextDetailsJson.id, {
+                                'assignees': [],
+                                'contextData': {
+                                    'contextMap': {
+                                        'classId': 'class-id-2'
+                                    },
+                                    'metadata': {
+                                        'title': 'Updated title'
+                                    }
+                                }
+                            }, {json: true})
+                            .addHeader('lms-id', 'quizzes')
+                            .addHeader('profile-id', ownerProfileJson.id)
+                            .inspectRequest()
+
+                            .expectStatus(200)
+                            .afterJSON(function (updatedContextJson) {
+                                frisby.create('Verify that the context data was updated')
+                                    .get(QuizzesApiUrl + '/v1/context/created/' + updatedContextJson.id)
+                                    .addHeader('lms-id', 'quizzes')
+                                    .addHeader('profile-id', ownerProfileJson.id)
+                                    .inspectRequest()
+
+                                    .expectStatus(200)
+                                    .inspectJSON()
+                                    .expectJSON({
+                                        "assignees": function (val) {
+                                            expect(val.length).toBe(2)
+                                        },
+                                        'contextData': {
+                                            'contextMap': {
+                                                'classId': 'class-id-1'
+                                            },
+                                            'metadata': {
+                                                'title': 'Updated title',
+                                                'startDate': 0,
+                                                'dueDate': 0
+                                            }
+                                        },
+                                        "id": updatedContextJson.id
+                                    })
+                                    .toss();
+                            })
+                            .toss();
+                    })
+                    .toss();
+            })
+            .toss();
+    })
+    .toss();
