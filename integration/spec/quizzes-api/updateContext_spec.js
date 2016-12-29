e082a6e0306cbe5b64b37c9fcffbd3757c3b7199
mzumbadoate <mzumbado@stormpath.com>
2016-12-29 09:17:30 -0600

    QZ-308 fixing the IT checking owner profile after context creation

--- integration/spec/quizzes-api/updateContext_spec.js
+++ integration/spec/quizzes-api/updateContext_spec.js
@@ -1,59 +1,59 @@
 const QuizzesApiUrl = require('./quizzesTestConfiguration.js').quizzesApiUrl;
 var frisby = require('frisby');
 
-frisby.create('Gets the owner profileId')
-    .get(QuizzesApiUrl + '/v1/profile-by-external-id/teacher-id-1')
+frisby.create('Test Context create with two assignees')
+    .post(QuizzesApiUrl + '/v1/context', {
+        'externalCollectionId': '927ec170-f7b3-46c3-ace3-24fd61dda0c0',
+        'assignees': [
+            {
+                'id': 'student-id-1',
+                'firstName': 'StudentFirstName1',
+                'lastName': 'StudentLastName1',
+                'username': 'student1',
+                'email': 'student1@quizzes.com'
+            },
+            {
+                'id': 'student-id-2',
+                'firstName': 'StudentFirstName2',
+                'lastName': 'StudentLastName2',
+                'username': 'student2',
+                'email': 'student2@quizzes.com'
+            }
+
+        ],
+        'contextData': {
+            'contextMap': {
+                'classId': 'class-id-1'
+            },
+            'metadata': {}
+        },
+        'owner': {
+            'id': 'teacher-id-1',
+            'firstName': 'TeacherFirstName1',
+            'lastName': 'TeacherLastName1',
+            'username': 'teacher1',
+            'email': 'teacher1@quizzes.com'
+        }
+    }, {json: true})
+    // This profile-id header for this specific endpoint is fake, it is useless but it is required by the endpoint
+    // Both headers will be replaced by the session-token header once we finish with the Session implementation
+    .addHeader('profile-id', '1fd8b1bc-65de-41ee-849c-9b6f339349c9')
     .addHeader('lms-id', 'quizzes')
+    .inspectRequest()
+
+    .expectStatus(200)
+    .expectHeaderContains('content-type', 'application/json')
     .inspectJSON()
-    .afterJSON(function(profileJson) {
-        frisby.create('Test Context update for one assignee')
-            .post(QuizzesApiUrl + '/v1/context', {
-                'externalCollectionId': '927ec170-f7b3-46c3-ace3-24fd61dda0c0',
-                'assignees': [
-                    {
-                        'id': 'student-id-1',
-                        'firstName': 'StudentFirstName1',
-                        'lastName': 'StudentLastName1',
-                        'username': 'student1',
-                        'email': 'student1@quizzes.com'
-                    },
-                    {
-                        'id': 'student-id-2',
-                        'firstName': 'StudentFirstName2',
-                        'lastName': 'StudentLastName2',
-                        'username': 'student2',
-                        'email': 'student2@quizzes.com'
-                    }
-
-                ],
-                'contextData': {
-                    'contextMap': {
-                        'classId': 'class-id-1'
-                    },
-                    'metadata': {}
-                },
-                'owner': {
-                    'id': 'teacher-id-1',
-                    'firstName': 'TeacherFirstName1',
-                    'lastName': 'TeacherLastName1',
-                    'username': 'teacher1',
-                    'email': 'teacher1@quizzes.com'
-                }
-            }, {json: true})
-            // This profile-id header for this specific endpoint is fake, it is useless but it is required by the endpoint
-            // Both headers will be replaced by the session-token header once we finish with the Session implementation
-            .addHeader('profile-id', profileJson.id)
+    .afterJSON(function(contextJson) {
+        frisby.create('Gets the owner profileId of the created context')
+            .get(QuizzesApiUrl + '/v1/profile-by-external-id/teacher-id-1')
             .addHeader('lms-id', 'quizzes')
-            .inspectRequest()
-
-            .expectStatus(200)
-            .expectHeaderContains('content-type', 'application/json')
             .inspectJSON()
-            .afterJSON(function(contextJson) {
+            .afterJSON(function(ownerProfileJson) {
                 frisby.create('Verify that the context exists as expected')
                     .get(QuizzesApiUrl + '/v1/context/created/' + contextJson.id)
                     .addHeader('lms-id', 'quizzes')
-                    .addHeader('profile-id', profileJson.id)
+                    .addHeader('profile-id', ownerProfileJson.id)
                     .inspectRequest()
 
                     .expectStatus(200)
@@ -74,7 +74,7 @@ frisby.create('Gets the owner profileId')
                         'createdDate': function(val) {expect(val).toBeType(Number)}
                     })
                     .afterJSON(function (contextDetailsJson) {
-                        frisby.create('Updating the context')
+                        frisby.create('Updating the context to add 3 more assignees but 1 is already assigned')
                             .put(QuizzesApiUrl + '/v1/context/' + contextDetailsJson.id, {
                                 'assignees': [
                                     {
@@ -110,15 +110,16 @@ frisby.create('Gets the owner profileId')
                                 }
                             }, {json: true})
                             .addHeader('lms-id', 'quizzes')
-                            .addHeader('profile-id', profileJson.id)
+                            .addHeader('profile-id', ownerProfileJson.id)
                             .inspectRequest()
 
                             .expectStatus(200)
                             .afterJSON(function(updatedContextJson) {
-                                frisby.create('Verify that the context exists as expected')
+                                frisby.create('Verify that the context was updated ' +
+                                    'with 2 more assignees and data updated')
                                     .get(QuizzesApiUrl + '/v1/context/created/' + updatedContextJson.id)
                                     .addHeader('lms-id', 'quizzes')
-                                    .addHeader('profile-id', profileJson.id)
+                                    .addHeader('profile-id', ownerProfileJson.id)
                                     .inspectRequest()
 
                                     .expectStatus(200)
