cd47583309af33fd9e97036370ace7eef29cc14b
Fabian Perez <fabian@gooru.org>
2017-01-05 14:12:29 -0600

    Removing unnecessary code

--- src/main/java/com/quizzes/api/common/controller/ContextController.java
+++ src/main/java/com/quizzes/api/common/controller/ContextController.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
