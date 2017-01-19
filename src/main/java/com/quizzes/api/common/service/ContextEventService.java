496bc2e0814f29a7289cb27d0b8120ba1b42e2de
Fabian Perez <fabian@gooru.org>
2017-01-19 14:33:58 -0600

    Removing unnecessary import of collection

--- src/main/java/com/quizzes/api/common/service/ContextEventService.java
+++ src/main/java/com/quizzes/api/common/service/ContextEventService.java
@@ -29,7 +29,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
