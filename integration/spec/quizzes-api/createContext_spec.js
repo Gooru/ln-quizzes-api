cdbec75cc2e1df94b6e00b7d48a880f9c9029cd4
Fabian Perez <fabian@gooru.org>
2017-01-19 14:27:01 -0600

    Adding startTest in createContext

--- integration/spec/quizzes-api/createContext_spec.js
+++ integration/spec/quizzes-api/createContext_spec.js
@@ -2,24 +2,26 @@ const QuizzesApiUrl = require('./quizzesTestConfiguration.js').quizzesApiUrl;
 const QuizzesCommon = require('./quizzesCommon.js');
 var frisby = require('frisby');
 
-QuizzesCommon.createContext(function () {
-    QuizzesCommon.getProfileByExternalId('student-id-1', function(json){
-        frisby.create('Verify that Assignee Profile has the correct data')
-            .get(QuizzesApiUrl + '/v1/profile/' + json.id)
-            .addHeader('profile-id', json.id)
-            .addHeader('client-id', 'quizzes')
-            .inspectRequest()
+QuizzesCommon.startTest("Create Context", function () {
+    QuizzesCommon.createContext(function () {
+        QuizzesCommon.getProfileByExternalId('student-id-1', function (json) {
+            frisby.create('Verify that Assignee Profile has the correct data')
+                .get(QuizzesApiUrl + '/v1/profile/' + json.id)
+                .addHeader('profile-id', json.id)
+                .addHeader('client-id', 'quizzes')
+                .inspectRequest()
 
-            .expectStatus(200)
-            .inspectJSON()
-            .expectJSON({
-                'id': json.id,
-                'externalId': 'student-id-1',
-                'firstName': 'StudentFirstName1',
-                'lastName': 'StudentLastName1',
-                'username': 'student1',
-                'email': 'student1@quizzes.com'
-            })
-            .toss();
+                .expectStatus(200)
+                .inspectJSON()
+                .expectJSON({
+                    'id': json.id,
+                    'externalId': 'student-id-1',
+                    'firstName': 'StudentFirstName1',
+                    'lastName': 'StudentLastName1',
+                    'username': 'student1',
+                    'email': 'student1@quizzes.com'
+                })
+                .toss();
+        });
     });
 });
