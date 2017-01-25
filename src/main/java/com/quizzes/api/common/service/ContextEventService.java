df228bc005985cfd4e346ab2469b9809a235265a
Javier Camacho <javier.camacho@edify.cr>
2017-01-24 19:09:06 -0600

    QZ-392 Remove unused import

--- src/main/java/com/quizzes/api/common/service/ContextEventService.java
+++ src/main/java/com/quizzes/api/common/service/ContextEventService.java
@@ -28,7 +28,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.swing.text.AbstractDocument;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
