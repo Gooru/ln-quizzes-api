5cba9518387cfe4541853f5ac64778429a63a48c
Jeffrey A. Bermúdez P <jeffrey.bermudez@edify.cr>
2017-01-30 16:43:55 -0600

    QZ-426: Modify Quizzes DB Schema and removing non-required code.

--- src/test/java/com/quizzes/api/core/services/ContextEventServiceTest.java
+++ src/test/java/com/quizzes/api/core/services/ContextEventServiceTest.java
@@ -35,7 +35,6 @@ import org.springframework.boot.json.JsonParser;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
