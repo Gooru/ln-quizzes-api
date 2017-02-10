f93d94e390f3cf8aac0a1a90738e2978d525b5fb
Javier Camacho <javier.camacho@edify.cr>
2017-02-10 16:43:14 -0600

    QZ-459 Add drag and drop functional tests

--- integration/spec/quizzes-api/questionsTypes_spec.js
+++ integration/spec/quizzes-api/questionsTypes_spec.js
@@ -29,11 +29,43 @@ var checkTrueFalseQuestion = function (resources) {
     });
 };
 
+var checkDragAndDropQuestion = function (resources) {
+    expect(resources.length).toEqual(1);
+    expect(resources[0].metadata.title).toEqual("Drag and Drop Question");
+    expect(resources[0].metadata.body).toEqual("Drag and Drop Question");
+    expect(resources[0].metadata.correctAnswer[0]).toEqual({"value": "T25l"});
+    expect(resources[0].metadata.correctAnswer[1]).toEqual({"value": "VHdv"});
+    expect(resources[0].metadata.correctAnswer[2]).toEqual({"value": "VGhyZWU="});
+    expect(resources[0].metadata.interaction.shuffle).toBeDefined();
+    expect(resources[0].metadata.interaction.maxChoices).toBeDefined();
+    expect(resources[0].metadata.interaction.prompt).toBeDefined();
+    expect(resources[0].metadata.interaction.choices.length).toEqual(3);
+    expect(resources[0].metadata.interaction.choices).toContain({
+        "text": "One",
+        "isFixed": true,
+        "value": "T25l",
+        "sequence": 1
+    });
+    expect(resources[0].metadata.interaction.choices).toContain({
+        "text": "Two",
+        "isFixed": true,
+        "value": "VHdv",
+        "sequence": 2
+    });
+    expect(resources[0].metadata.interaction.choices).toContain({
+        "text": "Three",
+        "isFixed": true,
+        "value": "VGhyZWU=",
+        "sequence": 3
+    });
+};
+
 
 QuizzesCommon.startTest("Get an assessment and check all the question types", function () {
     QuizzesCommon.getAssessmentById(QuizzesCommon.questionTypeDemoAssessment, function(json) {
         var resources = json.resources;
 
+        checkDragAndDropQuestion(getResourceByQuestionType(resources, "drag_and_drop"));
         checkTrueFalseQuestion(getResourceByQuestionType(resources, "true_false"));
     });
 });
