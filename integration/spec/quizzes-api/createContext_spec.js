8b9e24da053df61a0d2b0d3f8b2a64d14bbb63a4
Javier Camacho <javier.camacho@edify.cr>
2017-02-21 10:13:15 -0600

    QZ-500 Improve createContext functional test code

--- integration/spec/quizzes-api/createContext_spec.js
+++ integration/spec/quizzes-api/createContext_spec.js
@@ -1,8 +1,8 @@
 const QuizzesCommon = require('./quizzesCommon.js');
-const HttpErrorCodes = require('./quizzesCommon.js').httpErrorCodes;
+const HttpErrorCodes = QuizzesCommon.httpErrorCodes;
 const Config = require('./quizzesTestConfiguration.js');
 
-var baseContext = {
+let baseContext = {
     'collectionId': Config.getCollection('TestCollection01').id,
     'classId': Config.getClass('TestClass01').id,
     'isCollection': true,
@@ -13,47 +13,47 @@ var baseContext = {
     }
 };
 
-QuizzesCommon.startTest("Create context with wrong collection id", function () {
-    QuizzesCommon.getAuthorizationToken("Teacher01", function (authToken) {
-        var context = Object.assign({}, baseContext);
+QuizzesCommon.startTest('Create context with wrong collection id', function () {
+    QuizzesCommon.getAuthorizationToken('Teacher01', function (authToken) {
+        let context = Object.assign({}, baseContext);
         context.collectionId = 'wrong-assessment-id';
 
-        QuizzesCommon.verifyHttpErrorPost("Create context with wrong collection id",
+        QuizzesCommon.verifyHttpErrorPost('Create context with wrong collection id',
             '/v1/contexts', context, HttpErrorCodes.BAD_REQUEST,  authToken);
     });
 });
 
-QuizzesCommon.startTest("Create context with not existing collection id", function () {
-    QuizzesCommon.getAuthorizationToken("Teacher01", function (authToken) {
+QuizzesCommon.startTest('Create context with not existing collection id', function () {
+    QuizzesCommon.getAuthorizationToken('Teacher01', function (authToken) {
         let context = Object.assign({}, baseContext);
         context.collectionId = QuizzesCommon.generateUUID();
 
-        QuizzesCommon.verifyHttpErrorPost("Create context with not existing collection id",
+        QuizzesCommon.verifyHttpErrorPost('Create context with not existing collection id',
             '/v1/contexts', context, HttpErrorCodes.NOT_FOUND, authToken);
     });
 });
 
-QuizzesCommon.startTest("Create context without a collection id", function () {
-    QuizzesCommon.getAuthorizationToken("Teacher01", function (authToken) {
-        var context = Object.assign({}, baseContext);
+QuizzesCommon.startTest('Create context without a collection id', function () {
+    QuizzesCommon.getAuthorizationToken('Teacher01', function (authToken) {
+        let context = Object.assign({}, baseContext);
         context.collectionId = null;
 
-        QuizzesCommon.verifyHttpErrorPost("Create context without a collection id",
+        QuizzesCommon.verifyHttpErrorPost('Create context without a collection id',
             '/v1/contexts', context, HttpErrorCodes.BAD_REQUEST, authToken);
     });
 });
 
-QuizzesCommon.startTest("Create context without contextData", function () {
-    QuizzesCommon.getAuthorizationToken("Teacher01", function (authToken) {
-        var context = Object.assign({}, baseContext);
+QuizzesCommon.startTest('Create context without contextData', function () {
+    QuizzesCommon.getAuthorizationToken('Teacher01', function (authToken) {
+        let context = Object.assign({}, baseContext);
         context.contextData = null;
 
-        QuizzesCommon.verifyHttpErrorPost("Create context without contextData",
+        QuizzesCommon.verifyHttpErrorPost('Create context without contextData',
             '/v1/contexts', context, HttpErrorCodes.BAD_REQUEST, authToken);
     });
 });
 
-QuizzesCommon.startTest("Create context", function () {
+QuizzesCommon.startTest('Create context', function () {
     QuizzesCommon.getAuthorizationToken('Teacher01', function (authToken) {
         let collectionId = Config.getCollection('TestCollection01').id;
         let classId = Config.getClass('TestClass01').id;
@@ -64,7 +64,7 @@ QuizzesCommon.startTest("Create context", function () {
     });
 });
 
-QuizzesCommon.startTest("Create context with anonymous user", function () {
+QuizzesCommon.startTest('Create context with anonymous user', function () {
     QuizzesCommon.getAnonymousToken(function (authToken) {
         let collectionId = Config.getCollection('TestCollection01').id;
         let classId = Config.getClass('TestClass01').id;
