2f9a42b0f77e495f74b83c39cfaa43d2801aecd6
Javier Camacho <javier.camacho@edify.cr>
2017-02-21 15:33:45 -0600

    QZ-501 Add create context tests for assessments

--- integration/spec/quizzes-api/createContext_spec.js
+++ integration/spec/quizzes-api/createContext_spec.js
@@ -16,19 +16,23 @@ let baseContext = {
 QuizzesCommon.startTest('Create context with wrong collection id', function () {
     QuizzesCommon.getAuthorizationToken('Teacher01', function (authToken) {
         let context = Object.assign({}, baseContext);
-        context.collectionId = 'wrong-assessment-id';
+        context.collectionId = 'wrong-collection-id';
 
         QuizzesCommon.verifyHttpErrorPost('Create context with wrong collection id',
             '/v1/contexts', context, HttpErrorCodes.BAD_REQUEST,  authToken);
     });
 });
 
-QuizzesCommon.startTest('Create context with not existing collection id', function () {
+QuizzesCommon.startTest('Create context with not existing collection or assessment id', function () {
     QuizzesCommon.getAuthorizationToken('Teacher01', function (authToken) {
         let context = Object.assign({}, baseContext);
         context.collectionId = QuizzesCommon.generateUUID();
 
         QuizzesCommon.verifyHttpErrorPost('Create context with not existing collection id',
+            '/v1/contexts', context, HttpErrorCodes.NOT_FOUND, authToken);
+
+        context.isCollection = false;
+        QuizzesCommon.verifyHttpErrorPost('Create context with not existing assessment id',
             '/v1/contexts', context, HttpErrorCodes.NOT_FOUND, authToken);
     });
 });
@@ -39,6 +43,10 @@ QuizzesCommon.startTest('Create context without a collection id', function () {
         context.collectionId = null;
 
         QuizzesCommon.verifyHttpErrorPost('Create context without a collection id',
+            '/v1/contexts', context, HttpErrorCodes.BAD_REQUEST, authToken);
+
+        context.isCollection = false;
+        QuizzesCommon.verifyHttpErrorPost('Create context without a assessment id',
             '/v1/contexts', context, HttpErrorCodes.BAD_REQUEST, authToken);
     });
 });
@@ -53,11 +61,22 @@ QuizzesCommon.startTest('Create context without contextData', function () {
     });
 });
 
-QuizzesCommon.startTest('Create context', function () {
+QuizzesCommon.startTest('Create context with collection', function () {
     QuizzesCommon.getAuthorizationToken('Teacher01', function (authToken) {
         let collectionId = Config.getCollection('TestCollection01').id;
         let classId = Config.getClass('TestClass01').id;
         QuizzesCommon.createContext(collectionId, classId, true, {}, authToken, function (contextResponse) {
+            expect(contextResponse).not.toBe(null);
+            expect(contextResponse.id).toBeDefined();
+        });
+    });
+});
+
+QuizzesCommon.startTest('Create context with assessment', function () {
+    QuizzesCommon.getAuthorizationToken('Teacher01', function (authToken) {
+        let assessmentId = Config.getAssessment('TestAssessment01').id;
+        let classId = Config.getClass('TestClass01').id;
+        QuizzesCommon.createContext(assessmentId, classId, false, {}, authToken, function (contextResponse) {
             expect(contextResponse).not.toBe(null);
             expect(contextResponse.id).toBeDefined();
         });
