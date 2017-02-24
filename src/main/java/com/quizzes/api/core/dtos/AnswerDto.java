3489e28c6413aa098e3af2681d9ffc256b05fdc3
Javier Camacho <javier.camacho@gooru.org>
2017-02-24 15:54:17 -0600

    QZ-539 Fix fill in the blank scoring issue

--- src/main/java/com/quizzes/api/core/dtos/AnswerDto.java
+++ src/main/java/com/quizzes/api/core/dtos/AnswerDto.java
@@ -1,24 +1,16 @@
 package com.quizzes.api.core.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AnswerDto implements Serializable {
 
     private String value;
-
-    public AnswerDto() {
-    }
-
-    public AnswerDto(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
 
 }
