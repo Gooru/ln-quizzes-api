eff0c7d3ff9a273c7f4ff64f981402ecd2a979a3
Fabian Perez <fabian@gooru.org>
2017-03-08 14:18:52 -0600

    Requested changes

--- src/main/java/com/quizzes/api/core/dtos/content/ContextResourceEventContentDto.java
+++ src/main/java/com/quizzes/api/core/dtos/content/ContextResourceEventContentDto.java
@@ -26,8 +26,12 @@ public class ContextResourceEventContentDto extends ContextCommonEventContentDto
     private String resourceType;
 
     @Builder
-    public ContextResourceEventContentDto(UUID contentGooruId, String type, String collectionType, String collectionSubType, UUID courseGooruId, UUID classGooruId, UUID unitGooruId, UUID lessonGooruId, String clientSource, String source, UUID appId, UUID partnerId, UUID tenantId, UUID parentGooruId, UUID parentEventId, String resourceType) {
-        super(contentGooruId, type, collectionType, collectionSubType, courseGooruId, classGooruId, unitGooruId, lessonGooruId, clientSource, source, appId, partnerId, tenantId);
+    private ContextResourceEventContentDto(UUID contentGooruId, String type, String collectionType, String collectionSubType,
+                                          UUID courseGooruId, UUID classGooruId, UUID unitGooruId, UUID lessonGooruId,
+                                          String clientSource, String source, UUID appId, UUID partnerId, UUID tenantId,
+                                          UUID parentGooruId, UUID parentEventId, String resourceType) {
+        super(contentGooruId, type, collectionType, collectionSubType, courseGooruId, classGooruId, unitGooruId,
+                lessonGooruId, clientSource, source, appId, partnerId, tenantId);
         this.parentGooruId = parentGooruId;
         this.parentEventId = parentEventId;
         this.resourceType = resourceType;
