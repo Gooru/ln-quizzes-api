83fff5edae8610bbb1f70c3c556cf67c8a037c90
Javier Camacho <javier.camacho@gooru.org>
2017-03-09 11:51:53 -0600

    QZ-563 Update start context to create contet profile on first time use

--- src/main/java/com/quizzes/api/core/rest/clients/ClassMemberRestClient.java
+++ src/main/java/com/quizzes/api/core/rest/clients/ClassMemberRestClient.java
@@ -5,6 +5,7 @@ import com.quizzes.api.core.dtos.ClassMemberContentDto;
 import com.quizzes.api.core.exceptions.ContentNotFoundException;
 import com.quizzes.api.core.exceptions.ContentProviderException;
 import com.quizzes.api.core.exceptions.InternalServerException;
+import com.quizzes.api.core.exceptions.InvalidAssigneeException;
 import com.quizzes.api.core.services.ConfigurationService;
 import com.quizzes.api.core.services.content.helpers.GooruHelper;
 import org.slf4j.Logger;
@@ -63,6 +64,10 @@ public class ClassMemberRestClient {
             logger.error("Gooru class member for class ID: '" + classId + "' could not be retrieved.", hcee);
             if (hcee.getStatusCode().equals(HttpStatus.NOT_FOUND)) {
                 throw new ContentNotFoundException("Class member for class ID:" + classId + " could not be found.");
+            }
+            if (hcee.getStatusCode().equals(HttpStatus.FORBIDDEN)) {
+                throw new InvalidAssigneeException("Current user doesn't have permissions to access " +
+                        classId + " members.");
             }
             throw new ContentProviderException("Class member for class ID: " + classId + " could not be retrieved.", hcee);
         } catch (Exception e) {
