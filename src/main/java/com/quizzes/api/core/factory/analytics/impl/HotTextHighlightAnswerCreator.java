98a73d39e943fa4192fbab0b777df0b68978f76b
gmmendezp <gmmendezp@gmail.com>
2017-07-14 15:04:55 -0600

    NILE-1213. Fix highlight text answer being split incorrectly.

--- src/main/java/com/quizzes/api/core/factory/analytics/impl/HotTextHighlightAnswerCreator.java
+++ src/main/java/com/quizzes/api/core/factory/analytics/impl/HotTextHighlightAnswerCreator.java
@@ -9,6 +9,8 @@ import org.springframework.util.Assert;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class HotTextHighlightAnswerCreator implements AnswerCreator {
@@ -30,15 +32,17 @@ public class HotTextHighlightAnswerCreator implements AnswerCreator {
         List<String> userAnswers = getAnswerValues(answerResource.getAnswer());
 
         for (String answer : userAnswers) {
-            String[] answerParts = answer.split(",");
+            Pattern answerPattern = Pattern.compile("(.*),(.*)");
+            Matcher answerMatcher = answerPattern.matcher(answer);
+            answerMatcher.find();
 
             answerObjects.add(AnswerObject.builder()
                     .answerId("0")
                     .timeStamp(answerResource.getTimeSpent())
-                    .order(optionsOrder.indexOf(new Integer(answerParts[1])) + 1)
+                    .order(optionsOrder.indexOf(new Integer(answerMatcher.group(2))) + 1)
                     .status(isCorrectContains(answer, correctValues))
                     .skip(false)
-                    .text(answerParts[0])
+                    .text(answerMatcher.group(1))
                     .build());
         }
 
